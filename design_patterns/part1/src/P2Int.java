0bd6d6dd798426ddc7d482ba15b59d10f6f3a9a2
Mojtaba Eshghie <mojtaba.eshghi@ut.ac.ir>
2020-07-26 16:55:48 +0430

    session one finished

--- design_patterns/part1/src/P2Int.java
+++ design_patterns/part1/src/P2Int.java
@@ -26,3 +26,34 @@ class Projector implements displayModule {
         System.out.println("Displayed through a projector");
     }
 }
+
+// let's utilize the classes now:
+class Computer {
+    //using supertype (not concrete type):
+    displayModule dm;
+
+    // Now, this setDisplayModule can be invoked externally to set
+    // the display module of this computer
+    public void setDisplayModule (displayModule dm) {
+        this.dm = dm;
+    }
+
+    public void display() {
+        dm.display();
+    }
+}
+
+public class P2Int {
+    public static void main(String[] args) {
+
+        // creating a computer with a projector
+        Computer projectorComputer = new Computer();
+        projectorComputer.setDisplayModule(new Projector());
+        projectorComputer.display();
+
+        // creating a computer that has a monitor
+        Computer monitorComputer = new Computer();
+        monitorComputer.setDisplayModule(new Monitor());
+        monitorComputer.display();
+    }
+}
